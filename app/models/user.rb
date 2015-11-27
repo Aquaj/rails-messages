e44e71daa5f415b09dbb517d9afacbe946f48e4b
Aquaj <aquajvalin@gmail.com>
2015-11-27 12:11:24 +0100

    User system working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["name = :value OR lower(email) = lower(:value)", { :value => login }], validate: validate_username).first
+      else
+        where(conditions.to_hash).first
+      end
+    end
+
+  def validate_username
+  	if User.where(email: name).exists?
+    	errors.add(:name, :invalid)
+  	end
+  end
 end
