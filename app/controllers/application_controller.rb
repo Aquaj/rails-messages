e087ae4fbece5ffe758af7ea047aae077c477c6f
Aquaj <aquajvalin@gmail.com>
2015-11-28 18:28:52 +0100

    Templates for Message implemented.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :require_user
+
+  def require_user
+     redirect_to '/login' unless current_user
+   end 
 
   protected
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,46 @@
 class MessagesController < ApplicationController
+	before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
+
 	def index
 		@messages = Message.all
 	end
 	def show
+		@message = Message.find(params[:id])
 	end
 
 	def new
+		@message = Message.new
 	end
-	def index
+	def create
+		@message = Message.new
+		@message.author = current_user.login
+		if @message.update(message_params)
+			redirect_to message_path(@message.id)
+		else
+			render 'new'
+		end
 	end
 	
 	def edit
+		@message = Message.find(params[:id])
 	end
 	def update
+		@message = Message.find(params[:id])
+		if @message.update(message_params)
+			redirect_to @message
+		else
+			render 'edit'
+		end
 	end
 	
-	def delete
+	def destroy
+		@message = Message.find(params[:id])
+    	@message.destroy
+    	redirect_to root_url
 	end
+
+	private
+    def message_params
+      params.require(:message).permit(:content)
+    end
 end
